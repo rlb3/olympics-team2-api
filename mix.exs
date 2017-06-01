c3ea3777b27bb075332113a8c617efa89226f319
Robert Boone <robert@rlb3.com>
2017-06-01 10:10:38 -0500

    Add csv package

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule API.Mixfile do
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:csv, "~> 2.0.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
