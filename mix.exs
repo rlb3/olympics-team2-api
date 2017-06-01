2f6a0773c5de57ba2c2618df7e7b6991652a3bf8
Robert Boone <robert@rlb3.com>
2017-06-01 12:31:18 -0500

    Add absinthe

--- mix.exs
+++ mix.exs
@@ -28,13 +28,15 @@ defmodule API.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:phoenix, "~> 1.3.0-rc", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:csv, "~> 2.0.0"}]
+     {:csv, "~> 2.0.0"},
+     {:absinthe_plug, "~> 1.3"},
+     {:absinthe, "~> 1.3.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
