610e0e9e279f58feddbba4a1142ddcdf731e029f
Robert Boone <robert@rlb3.com>
2017-06-01 12:11:00 -0500

    Seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,35 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     API.Repo.insert!(%API.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# ["Sport", "Event", "Gold", "Silver", "Bronze", "Gold Country",
+#  "Gold Winner", "Silver Country", "Silver Winner", "Bronze Country",
+#  "Bronze Winner"]
+
+defmodule NameParser do
+  def parse(names) do
+    case Regex.scan(~r/[A-Z]+\s[A-Z]\S+/, String.trim(names)) do
+      [first, second, third] -> [first, second, third] |> Enum.join(", ")
+      [first, last] -> [first, last] |> Enum.join(", ")
+      [first] -> first |> Enum.join(", ")
+      [] -> names
+    end
+  end
+end
+
+defmodule CountryABV do
+  @codes File.stream!("country_codes.csv") |> CSV.decode! |> Enum.into([])
+
+  def find(abv) do
+    country_name= for [name, _, ^abv | _] <- @codes, do: name || [""]
+    case country_name do
+      [name] -> name
+      [] -> ""
+    end
+  end
+end
+
+csv_data = File.stream!("2016-summer-olympic-medals.csv") |> CSV.decode! |> Enum.into([])
+
+for [sport, event, gold_count, silver_count, bronze_count, gold_country, gold_winner, silver_country, silver_winner, bronze_country, bronze_winner] <- csv_data do
+    API.Repo.insert %API.Medal{sport: sport, event: event, gold: String.to_integer(gold_count), silver: String.to_integer(silver_count), bronze: String.to_integer(bronze_count), gold_country: CountryABV.find(gold_country), silver_country: CountryABV.find(silver_country), bronze_country: CountryABV.find(bronze_country), gold_winner: NameParser.parse(gold_winner), silver_winner: NameParser.parse(silver_winner), bronze_winner: NameParser.parse(bronze_winner)}
+end
+
+
+IO.puts CountryABV.find("FRA")
