c891a4ac56d912b72fa5da1ee68c091eb1020913
Robert Boone <robert@rlb3.com>
2017-06-02 10:36:51 -0500

    Fill out the GraphQL docs

--- lib/api/schema.ex
+++ lib/api/schema.ex
@@ -1,6 +1,5 @@
 defmodule API.Schema do
   use Absinthe.Schema
-  alias Absinthe.Type.{Field, Object}
   alias API.Repo
 
   @moduledoc """
@@ -24,6 +23,14 @@ defmodule API.Schema do
   - total_bronze: Total number ot silver medals for a country
   """
 
+  @desc """
+  - id: UUID
+  - title: String
+  - body: String
+  - photo_url: URL to image file
+  - photo_name: Image file name minus /images/
+  - date: Date of story
+  """
   object :top_news do
     field :id, :id
     field :title, :string, description: "News title"
@@ -33,6 +40,13 @@ defmodule API.Schema do
     field :date, :string, description: "Date"
   end
 
+  @desc """
+  - country: Country name
+  - total: Total number of medals for a country
+  - total_gold: Total number of gold medals for a country
+  - total_silver: Total number of silver medals for a country
+  - total_bronze: Total number ot silver medals for a country
+  """
   object :medal do
     field :country, :string, description: "Country name"
     field :total, :integer, description: "Total metals for the country"
@@ -41,9 +55,8 @@ defmodule API.Schema do
     field :total_bronze, :integer, description: "Total bronze metals for the country"
   end
 
-
   query do
-    field :medals, list_of(:medal) do
+    field :medals, list_of(:medal), description: "Metals and the tallies for total metals won, number of gold, silver and bronze metals" do
       resolve fn _arg, _context ->
         case API.Cache.get_medals do
           nil ->
@@ -59,7 +72,7 @@ defmodule API.Schema do
       end
     end
 
-    field :top_news, list_of(:top_news) do
+    field :top_news, list_of(:top_news), description: "Top news feed" do
       resolve fn _arg, _context ->
         case API.Cache.get_news do
           nil ->
